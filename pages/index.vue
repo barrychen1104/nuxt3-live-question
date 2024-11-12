c1bf234a8641f4d82277310f4aa50fee325f9bcc
BarryChen <s481838@sinyi.com.tw>
2024-11-12 18:11:44 +0800

    import bootstrap5 scss

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,24 @@
 
 <template>
   <div>
-    Page: 首頁
+    <h1>Page: index</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+
+    <button type="button" class="btn btn-link">Link</button>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+$primary:blue;
+h1{
+  color: $primary;
+  font-size: $fs-xl;
+} 
+</style>
