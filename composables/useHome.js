378d9bc174f0f1e0f2415674e4921e259a48d1e0
BarryChen <s481838@sinyi.com.tw>
2024-11-15 11:22:15 +0800

    add composables

--- composables/useHome.js
+++ composables/useHome.js
@@ -1,3 +1,30 @@
+import axios from "axios";
+
 export const useHome = () => {
-  return {};
+  const newsList = ref([]);
+  const isLoading = ref(false);
+
+  // API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
+  // 使用 ES6 fetch() 或是 axios.get() 串接 API
+  // 切換 isLoading 狀態
+  const fetchNews = async () => {
+    isLoading.value = true;
+
+    try {
+      const { data } = await axios.get(
+        "https://nuxr3.zeabur.app/api/v1/home/news/"
+      );
+      newsList.value = data?.result;
+    } catch (error) {
+      console.error("獲取新聞失敗:", error);
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return {
+    newsList,
+    isLoading,
+    fetchNews,
+  };
 };
--- pages/index.vue
+++ pages/index.vue
@@ -2,20 +2,15 @@
 import Loading from "vue-loading-overlay";
 import "vue-loading-overlay/dist/css/index.css";
 
-const newsList = ref([]);
-const isLoading = ref(false);
-
-// API 路徑 : https://nuxr3.zeabur.app/api/v1/home/news/
-// 使用 ES6 fetch() 或是 axios.get() 串接 API
-// 切換 isLoading 狀態
-
-
+const { newsList, isLoading, fetchNews } = useHome();
+fetchNews();
 </script>
 
 <template>
   <div class="container">
     <h1>最新消息</h1>
     <!-- <NewsCard v-for="..." :key="..."  v-bind="..." /> -->
+    <NewsCard v-for="news in newsList" :key="news.id" v-bind="news" />
 
     <ClientOnly>
       <Loading v-model:active="isLoading" />
