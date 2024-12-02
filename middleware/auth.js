019473fb2520bbb44014f0fc2159a50eb46ab752
BarryChen <s481838@sinyi.com.tw>
2024-12-02 11:29:19 +0800

    新增客戶端渲染檢查以防止重複 API 請求

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
+  const { isHydrating, payload } = useNuxtApp();
+  // 是否在客戶端、是否正在將伺服器端渲染的程式碼轉換成客戶端的、伺服器端是否以渲染過 HTML
+  if (import.meta.client && isHydrating && payload.serverRendered) {
+    return; // 阻止 $fetch 在客戶端再戳 API
+  }
+
   const token = useCookie("auth");
   if (!token) {
     return navigateTo("/login");
